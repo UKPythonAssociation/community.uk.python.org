83d005d36460896a9e8f4b911c745dc9835a7ab4
Peter Inglesby <peter.inglesby@gmail.com>
2017-01-01 17:07:39 +0000

    Add Event model

--- ukpython/models.py
+++ ukpython/models.py
@@ -10,3 +10,18 @@ class UserGroup(ModelWithoutContent):
 
     def __str__(self):
         return self.name
+
+
+class Event(ModelWithoutContent):
+    user_group = models.ForeignKey(UserGroup)
+    name = models.CharField(max_length=255)
+    url = models.URLField(null=True)
+    date = models.DateField(null=True)
+    time = models.TimeField(null=True)
+    venue = models.CharField(max_length=255, null=True)
+
+    dump_dir_path = 'events'
+    key_structure = '[user_group]/[date]'
+
+    def __str__(self):
+        return self.name
