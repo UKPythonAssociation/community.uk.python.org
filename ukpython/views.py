30edec401f714b68afbec2e0d137765fcc16729e
Peter Inglesby <peter.inglesby@gmail.com>
2017-02-23 15:51:50 +0000

    Add news list and detail pages

--- ukpython/views.py
+++ ukpython/views.py
@@ -1,4 +1,5 @@
 import calendar
+import datetime
 
 from django.shortcuts import render
 
@@ -41,3 +42,20 @@ def user_groups_with_no_events_scheduled(request, year, month):
         'year': year,
     }
     return render(request, 'ukpython/user_groups_with_no_events_scheduled.html', context)
+
+
+def news(request):
+    context = {
+        'news_items': NewsItem.objects.all()
+    }
+    return render(request, 'ukpython/news.html', context)
+
+
+def news_item(request, year, month, day, slug):
+    date = datetime.date(int(year), int(month), int(day))
+    news_item = NewsItem.objects.get(slug=slug, date=date)
+
+    context = {
+        'news_item': news_item,
+    }
+    return render(request, 'ukpython/news_item.html', context)
